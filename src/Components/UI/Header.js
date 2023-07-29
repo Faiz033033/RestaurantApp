b4b10d50f670ce60e80be67fb25eced11200b854
faiz033033 <feju302@gmail.com>
2023-07-29 09:01:51 +0530

    HeaderInitialize

---
+++ src/Components/UI/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div>
+                <h2>My Restro</h2>
+            </div>
+            <div>
+                <h3>Cart</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
