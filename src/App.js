7399458510133ed532856c1f6b8c42d1fad8c15a
faiz033033 <feju302@gmail.com>
2023-08-05 15:10:22 +0530

    ContextApiAdded

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React,{useState}  from 'react';
-import { Fragment } from 'react';
+import { useState } from 'react';
 
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
 import Cart from './Components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  const showCartHanndler = () => {
+  const showCartHandler = () => {
     setCartIsShown(true);
   };
 
-  const hideCartHanndler = () => {
+  const hideCartHandler = () => {
     setCartIsShown(false);
   };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onHideCart = {hideCartHanndler} />}
-      <Header onShowCart = {showCartHanndler} />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
